608059b6d48d838e88190f4b33502074637faec1
DauThuy <dauthithuy99@gmail.com>
2020-09-10 19:44:25 +0700

    Bai 1: The FAQs application

---
+++ BT1/index.js
@@ -0,0 +1,22 @@
+document.getElementById("h21").onclick =function () {
+	if( document.getElementById("1").style.display == "none"){
+		document.getElementById("1").style.display = "block";
+	}else{
+		document.getElementById("1").style.display = "none";
+	}  
+};
+
+document.getElementById("h22").onclick =function () {
+	if( document.getElementById("2").style.display == "none"){
+		document.getElementById("2").style.display = "block";
+	}else{
+		document.getElementById("2").style.display = "none";
+	}
+};
+document.getElementById("h23").onclick =function () {
+	if( document.getElementById("3").style.display == "none"){
+		document.getElementById("3").style.display = "block";
+	}else{
+		document.getElementById("3").style.display = "none";
+	}
+};
